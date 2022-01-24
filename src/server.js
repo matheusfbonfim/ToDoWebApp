4aacf17b7eb90b07997c029d03f10a8ecdf55d5d
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-01-24 19:45:42 -0300

    Public Directory
    Criando pasta de arquivos estáticos (CSS, Imagens, Scripts) e habilitando no servidor

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ const express = require('express');
 // Executando express - Servidor
 const server = express();
 
+// Middlewware ("Homem no meio" -> Entre o get)
+// Habilita os arquivos estáticos
+// Servidor cria rota automatico para os arquivos públicos
+server.use(express.static("public"))
+
+// ======================================================
+// =============ROTAS====================================
+// ======================================================
 
 // Requisição GET - Página Home
 server.get("/", (request, response) => {
   // __dirname -> Caminho para enviar o HTML
   // Respondendo a page home
   return response.sendFile(__dirname + "/views/index.html");
-
 })
 
 // Ligando o servidor - Porta 3000
