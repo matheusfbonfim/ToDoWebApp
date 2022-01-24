affb7d690f272612dd618b7928ed55be7187127d
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-01-24 20:27:51 -0300

    Definição de rotas para todas páginas
    Definir rotas e arrumar os path para direcionamento

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,46 @@ const routes = express.Router();
 // =============ROTAS====================================
 // ======================================================
 
+// __dirname -> Caminho no diretorio para enviar o HTML
+const basePath = __dirname + '/views'
+
+// Requisição GET - Página Home
+routes.get("/", (request, response) => {
+  // Respondendo a page home
+  return response.sendFile(basePath + "/index.html");
+})
+
+// Requisição GET - Página Adicionar Item
+routes.get("/item", (request, response) => {
+  return response.sendFile(basePath + "/item.html");
+})
+
+// Requisição GET - Página Adiciionar List
+routes.get("/list", (request, response) => {
+  return response.sendFile(basePath + "/list.html");
+})
+
+
+// Requisição GET - Página Edita Item
+routes.get("/item/edit", (request, response) => {
+  return response.sendFile(basePath + "/item-edit.html");
+})
+
+// Requisição GET - Página Edita List
+routes.get("/list/edit", (request, response) => {
+  return response.sendFile(basePath + "/list-edit.html");
+})
+
+
+
+
 // Requisição GET - Página Home
 routes.get("/", (request, response) => {
   // __dirname -> Caminho para enviar o HTML
   // Respondendo a page home
   return response.sendFile(__dirname + "/views/index.html");
 })
+
 
 
 
