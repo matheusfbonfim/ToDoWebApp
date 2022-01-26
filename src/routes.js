468b7cad8c9d605375a121eac8bbd4dd6e766fb1
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-01-26 20:45:27 -0300

    Refatorando List - Object Literal
    Criando um Object Literal List, adicionando data, controllers e services. Ao fim integrando com as rotas

--- src/routes.js
+++ src/routes.js
@@ -14,34 +14,72 @@ const profile = {
   avatar: "https://media-exp1.licdn.com/dms/image/D4E35AQEroDYURewRXg/profile-framedphoto-shrink_800_800/0/1639072813228?e=1643076000&v=beta&t=rItBvDvcyTFHg0IMrYLSF2OfYAVorgHRtnhwIiokeZM"
 }
 
-// Listas
-const lists = [
-  {
-    id: 1,
-    name: "To Do",
-    itens: [
-      {
-        id: 1,
-        name:"Fazer ToDo para o desafio da V360"
-      },
-      {
-        id: 2,
-        name:"Não esquecer"
-      },
-
-    ]
+// Object Literal - Objeto com propriedades
+const List = {
+  // Dados das listas
+  data:[
+    {
+      id: 1,
+      name: "To Do",
+      itens: [
+        {
+          id: 1,
+          name:"Fazer ToDo para o desafio da V360"
+        },
+        {
+          id: 2,
+          name:"Não esquecer"
+        },
+      ]
+    },
+    {
+      id: 2,
+      name: "Lista 2",
+      itens: [
+        {
+          id: 1,
+          name:"Fazer ToDo para o desafio da V360"
+        },
+      ]
+    }
+  ], 
+  // Controles
+  controllers:{
+    index(req, res){
+      // Respondendo a page home
+      // Passando para dentro da pagina as informações do "banco"
+      return response.render(views + "index", {lists: List.data});
+    },
+    create(req, res){
+      return response.render(views + "list");
+    },
+    createItem(req,res){
+      return response.render(views + "item");
+    },
+    save(req,res){
+      // request.body = {name: "dasdsa"}
+      const lastId = List.data[List.data.length - 1] ? List.data[List.data.length - 1] : 1;
+    
+      lists.push({
+        id: lastId + 1,
+        name:request.body.name,
+        itens: []});     // ADD na lista
+    
+      return response.redirect("/") // Redireciona para /
+    },
+    saveItem(req,res){
+      // ADD Item
+      List.data[0].itens.push({name:request.body.name})
+      console.log(List.data)   
+      return response.redirect("/") // Redireciona para /
+    }
   },
-  {
-    id: 2,
-    name: "Lista 2",
-    itens: [
-      {
-        id: 1,
-        name:"Fazer ToDo para o desafio da V360"
-      },
-    ]
-  }
-]
+  // Serviços
+  services:{
+  },
+}
+
+
 
 // ------------------------------------------------------
 // ======================================================
@@ -56,22 +94,13 @@ const views = __dirname + '/views/'  // -> EJS ja sabe que está nesse caminho (
 // GET
 
 // Requisição GET - Página Home
-routes.get("/", (request, response) => {
-  // Respondendo a page home
-  // Passando para dentro da pagina as informações do "banco"
-  return response.render(views + "index", {lists});
-})
+routes.get("/", List.controllers.index)
 
 // Requisição GET - Página Adicionar Item
-routes.get("/item", (request, response) => {
-  return response.render(views + "item");
-})
+routes.get("/item", List.controllers.createItem)
 
 // Requisição GET - Página Adiciionar List
-routes.get("/list", (request, response) => {
-  return response.render(views + "list");
-})
-
+routes.get("/list", List.controllers.create)
 
 // Requisição GET - Página Editar Item
 routes.get("/item/edit", (request, response) => {
@@ -93,25 +122,10 @@ routes.get("/profile", (request, response) => {
 // POST
 
 // Requisição POST - Página ADD List
-routes.post("/list", (request, response) => {
-  // request.body = {name: "dasdsa"}
-  const lastId = lists[lists.length - 1] ? lists[lists.length - 1] : 1;
-
-  lists.push({
-    id: lastId + 1,
-    name:request.body.name,
-    itens: []});     // ADD na lista
-
-  return response.redirect("/") // Redireciona para /
-})
+routes.post("/list", List.controllers.save)
 
 // Requisição POST - Página Item
-routes.post("/item", (request, response) => {
-  // ADD Item
-  lists[0].itens.push({name:request.body.name})
-  console.log(lists)   
-  return response.redirect("/") // Redireciona para /
-})
+routes.post("/item", List.controllers.saveItem)
 
 
 
